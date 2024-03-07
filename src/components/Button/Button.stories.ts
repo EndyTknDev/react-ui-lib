ceaea39e97cebb405a94f4d3663d1a7d0553e77f
Endy Mitsuya Takano <endytkndev@gmail.com>
2024-03-07 19:18:20 -0400

    feat: adding box's HOC on button

--- src/components/Button/Button.stories.ts
+++ src/components/Button/Button.stories.ts
@@ -1,10 +1,32 @@
-import type { Meta } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/react";
 import Button from "./Button";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: "Button",
   component: Button,
+  args: {
+    children: "value",
+  },
 } satisfies Meta<typeof Button>;
 
 export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Filled: Story = {
+  args: {
+    boxVariant: "inverted",
+  },
+};
+
+export const Inverted: Story = {
+  args: {
+    boxVariant: "inverted",
+  },
+};
+
+export const Underlined: Story = {
+  args: {
+    boxVariant: "underlined",
+  },
+};
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,19 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from "react";
+import styled from "styled-components";
 import { ButtonType } from "../../types/HTMLTypes";
+import withBox from "../Box/WithBox";
 
-const Button: React.FC<ButtonType> = React.forwardRef<HTMLButtonElement, ButtonType>(
+export const BaseButton: React.FC<ButtonType> = React.forwardRef<HTMLButtonElement, ButtonType>(
   ({ type, children, ...props }, ref) => (
     <button ref={ref} type={type} {...props}>
       {children}
     </button>
   ),
 );
+
+const Button = styled(withBox<ButtonType>(BaseButton))`
+  all: unset;
+`;
 
 export default Button;
